ac8ddba454274c8d6afccd0929cdff24d351373a
Kesava Jawaharlal <k@KesavaMBP.local>
2017-05-19 10:54:00 +0100

    Failing tests for Price implementation.

--- JohnLewisTest/JohnLewisTest/Source/Models/Product.swift
+++ JohnLewisTest/JohnLewisTest/Source/Models/Product.swift
@@ -8,27 +8,48 @@
 
 import Foundation
 
+fileprivate enum ParsingKey {
+    static let productId = "productId"
+    static let title = "title"
+    static let image = "image"
+    static let price = "price"
+    static let value = "now"
+    static let currency = "currency"
+}
 struct Product {
     var productId: Int
     var title: String
     var imageUrl: URL
-    var price: Price?
+    var price: Price
     
     init?(from json: [String:Any]) {
-        guard let productIdString = json["productId"] as? String,
+        guard let productIdString = json[ParsingKey.productId] as? String,
             let productId = Int(productIdString),
-            let title = json["title"] as? String,
-            let imageUrlString = json["image"] as? String,
-            let imageUrl = URL(string: imageUrlString) else {
+            let title = json[ParsingKey.title] as? String,
+            let imageUrlString = json[ParsingKey.image] as? String,
+            let imageUrl = URL(string: imageUrlString),
+            let priceDict = json[ParsingKey.price] as? [String: Any],
+            let price = Price(from: priceDict) else {
                 return nil
             }
         self.productId = productId
         self.title =  title
         self.imageUrl = imageUrl
+        self.price = price
     }
 }
 
 struct Price {
     var value: Float
     var currencyISOCode: String
+    
+    init?(from json: [String:Any]) {
+        guard let valueString = json[ParsingKey.value] as? String,
+            let value = Float(valueString),
+            let currencyISOCode = json[ParsingKey.currency] as? String else {
+                return nil
+        }
+        self.value = value
+        self.currencyISOCode = currencyISOCode
+    }
 }
